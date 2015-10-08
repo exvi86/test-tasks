a2e1c70d48e7c36e5e91e9a015ab9330348ff5c7
Vladimir Kichaev <exclusive.virus@exvi.hack>
2015-10-08 12:54:28 +0300

    Fixed cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,7 @@
 jboss_source = node['jboss']['source']
 jboss_file = node['jboss']['file']
 jboss_path = node['jboss']['path']
+jboss_user = node['jboss']['user']
 listen_ip = node['jboss']['listen_ip']
 app_source = node['app']['source']
 app_path = node['app']['path']
@@ -18,37 +19,53 @@ app_file = node['app']['file']
 remote_file "#{jboss_file}" do
     not_if {File.exists?(jboss_path)}
     source "#{jboss_source}"
-    owner 'vagrant'
-    group 'vagrant'
+    owner "#{jboss_user}"
+    group "#{jboss_user}"
     mode '0755'
     action :create
 end
 
 execute 'unzip_jboss' do
     not_if {File.exists?(jboss_path)}
-    command '/bin/unzip #{jboss_file} -d "#{jboss_path}"'
+    command "/bin/unzip #{jboss_file} -d #{jboss_path} && /bin/chown -R #{jboss_user}:#{jboss_user} #{jboss_path}"
+end
+
+template "/usr/lib/systemd/system/jboss.service" do
+    source "jboss.service.erb"
+    owner 'root'
+    group 'root'
+    mode '0755'
+    variables({
+        :jboss_user => jboss_user,
+        :jboss_path => jboss_path
+    })
+
+template "#{jboss_path}/jboss-eap-6.4/standalone/configuration/standalone.xml" do
+    source "standalone.xml.erb"
+    owner "#{jboss_user}"
+    group "#{jboss_user}"
+    mode '0755'
+    variables({
+        :listenip => listen_ip
+        })
+end
+
+service 'jboss' do
+    action :nothing
 end
 
 remote_file "#{app_file}" do
     not_if {File.exists?(app_path)}
     source "#{app_source}"
-    owner 'vagrant'
-    group 'vagrant'
+    owner "#{jboss_user}"
+    group "#{jboss_user}"
     mode '0755'
     action :create
 end
 
 execute 'unzip_app' do
     not_if {File.exists?(app_path)}
-    command '/bin/unzip #{app_file} -d #{app_path}'
+    command "/bin/unzip #{app_file} -d #{app_path} && /bin/chown -R #{jboss_user}:#{jboss_user} #{app_path}"
+    notifies :restart, 'service[jboss]', :immediately
 end
 
-template "#{jboss_path}/standalone/configuration/standalone.xml" do
-    source "standalone.xml.erb"
-    owner "vagrant"
-    group "vagrant"
-    mode "0755"
-    variables({
-	:listenip => listen_ip
-	})
-end
